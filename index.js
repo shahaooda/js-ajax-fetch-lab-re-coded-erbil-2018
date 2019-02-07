dbca521c585bac17debe60a31555a3abe82f327a
Shahad Bakoos <shahadbakoos@gmail.com>
2019-02-07 14:08:24 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,22 @@
+function getIssues() {
+}
+
+function showIssues(json) {
+}
+
+function createIssue() {
+}
+
+function showResults(json) {
+}
+
+function forkRepo() {
+  const repo = 'learn-co-curriculum/javascript-fetch-lab'
+  //use fetch to fork it!
+}
+
+function getToken() {
+  //change to your token to run in browser, but set
+  //back to '' before committing so all tests pass
+  return ''
+}
