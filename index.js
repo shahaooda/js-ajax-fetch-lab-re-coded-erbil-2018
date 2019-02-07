4dd2de71ce03af8e2bd4595b8dfda889ec2027e2
Shahad Bakoos <shahadbakoos@gmail.com>
2019-02-07 14:08:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,22 +1,4 @@
-const userName = ''
-const baseApi = 'https://api.github.com/'
-const fork = `${userName}/javascript-fetch-lab`
 
-//Issue and Repo objects and templates
-
-function Issue(attributes){
-  this.title = attributes.title;
-  this.body = attributes.body;
-  this.url = attributes.url;
-}
-
-function Repo(attributes){
-  this.url = attributes.url;
-}
-
-Issue.prototype.template = function(){
-   var template = `<li>Title: <a href="${this.url}">${this.title} </a><span> | Body: ${this.body}</span></li>`
-   return template;
 };
 
 Repo.prototype.template = function(){
